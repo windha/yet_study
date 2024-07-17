750b0eb8c315ef6f291599a8d77ab11ba5b37fee
root <root@localhost.localdomain>
2024-07-17 20:36:43 +0800

    update

---
+++ selenium/test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome()
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+title = driver.title
+
+driver.implicitly_wait(0.5)
+
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+text_box.send_keys("Selenium")
+submit_button.click()
+
+message = driver.find_element(by=By.ID, value="message")
+text = message.text
+
+driver.quit()
